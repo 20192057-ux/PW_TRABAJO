685ee6de241972a2d06a186cc327cdc3992ebf05
Sebastian Lázaro Torres <manutd796@gmail.com>
2025-10-13 14:58:19 -0500

    Nosotros

--- src/components/TeamMember.tsx
+++ src/components/TeamMember.tsx
@@ -1,22 +1,12 @@
-const TeamMember = (props : TeamMemberProps) => {
-    return (
-        <div className="col-md-3 text-center">
-            <img
-                src={props.src}
-                alt={props.alt || props.name}
-                className="rounded-circle mb-2"
-                style={{ width: props.size, height: props.size, objectFit: 'cover' }}
-            />
-            <p>{props.name}</p>
-        </div>
-    )
+function TeamMember(props: TeamMemberProps) {
+    
 }
 
 type TeamMemberProps = {
-  name: string
-  src: string
-  alt?: string
-  size?: number
+    name: string
+    src: string
+    alt?: string
+    size?: number
 }
 
 export default TeamMember
