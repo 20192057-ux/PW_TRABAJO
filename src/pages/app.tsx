53bfe65a49fa5e98e93ddc19594e381ff4a12469
Sebastian Valiente <20192057@aloe.ulima.edu.pe>
2025-10-14 22:05:26 -0500

    Agregado dashboarddelstreamer con horas detransmision

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import StreamerDashboard from "../components/StreamerDashboard";
 const App = () => {
     return (
         <div>
