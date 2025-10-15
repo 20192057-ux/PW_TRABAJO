42e171948351577445ce3d74ef466b8512528f88
Sebastian Valiente <20192057@aloe.ulima.edu.pe>
2025-10-14 22:27:20 -0500

    aa

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,13 +1,15 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import StreamerDashboard from "../components/StreamerDashboard";
-const App = () => {
-    return (
-        <div>
-            <Header/>
-        </div>
+import StreamerDashboard from "../components/StreamerDashboard"
 
-    )
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <StreamerDashboard />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
